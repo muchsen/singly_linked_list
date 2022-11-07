65254cea523ec5c1c3d481008251f29a074769a9
TA1059 <muchsen.ft21@mail.umy.ac.id>
2022-11-07 16:50:32 +0700

    Add project files.

---
+++ singly_linked_list/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace singly_linked_list
+{
+    //each node list of the information part and link to the next mode
+    class node
+    {
+        public int rollNumber;
+        public string name;
+        public node next;
+    }
+
+    class list
+    {
+
+    }
+
+    class program
+    {
+        static void main(string
+    }
+}
