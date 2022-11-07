fdb02ea7f254cc307e1a059099b7ade6daf2ebc6
TA1059 <muchsen.ft21@mail.umy.ac.id>
2022-11-07 17:15:10 +0700

    create public list and else mode and add note add if else mode

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -26,12 +26,39 @@ namespace singly_linked_list
             Console.WriteLine("\nEnter the roll number of the student: ");
             rollNo = Convert.ToInt32(Console.ReadLine());
             nm = Console.ReadLine();
+            node newnode = new node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            //if trhe node to be interested is the first node
+            if((START != null) || (rollNo == START.rollNumber))
+                {
+                if ((START != null) && (rollNo == START.rollNumber))
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                newnode.next = START;
+                START = newnode;
+                return;
+
+            }
+            node previous, current;
+            previous = START;
+            current = START;
+            while ((current != null) && (rollNo >= current.rollNumber))
+            {
+                Console.WriteLine();
+                return ;
+            }
+            previous.next = current;
+            previous.next = newnode;
         }
+        newnode
 
     }
 
     class program
     {
-        static void main(string
+        
     }
 }
